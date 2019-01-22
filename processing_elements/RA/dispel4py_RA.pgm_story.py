c98488a74a540d9a0d2d98b7d355d57336dd6dad
akrause2014 <a.krause@epcc.ed.ac.uk>
2019-01-22 13:02:41 +0000

    almost working

--- processing_elements/RA/dispel4py_RA.pgm_story.py
+++ processing_elements/RA/dispel4py_RA.pgm_story.py
@@ -3,6 +3,10 @@ Execution:
 Real --> dispel4py simple dispel4py_RA.pgm_story.py -d '{"streamProducer": [ {"input": "IV.MA9..HHR.START.OTLOC.SAC.20.50.real"} ] }'
 Synth --> dispel4py simple dispel4py_RA.pgm_story.py -d '{"streamProducer": [ {"input": "IV.MA9.HXR.semv.sac.20.50.synt"} ] }'
 
+
+Comparison:
+dispel4py simple dispel4py_RA.pgm_story.py -d '{"streamProducerReal": [ {"input": "IV.MA9..HHR.START.OTLOC.SAC.20.50.real"} ], "streamProducerSynth": [ {"input": "IV.MA9.HXR.semv.sac.20.50.synt"} ]   }'
+
 Output:
 WriteStream3: output_data is {'GroundMotion': {'stream': 'IV.MA9..HHR.START.OTLOC.SAC.20.50.real', 'ty': 'velocity', 'p_norm': 'max', 'pgd': '0.0006945877', 'pgv': '0.0002320527', 'pga': '0.00013708159', 'dmp_spec_acc': '0.00032428280150622804'}}
 
@@ -20,6 +24,7 @@ import math
 import numpy as np
 import os
 import json
+from collections import defaultdict
 
 def calculate_norm(stream):
     station = stream[0].stats.station
@@ -51,17 +56,6 @@ def calculate_norm(stream):
 
     return data_mean, data_max, d
 
-
-class StreamProducer(IterativePE):
-
-    def __init__(self):
-        IterativePE.__init__(self)
-
-    def _process(self, input):
-        filename = input
-        self.write('output', [read(filename), filename])
-
-
 def calculate_pgm(data, ty, delta):
     pgm = max(abs(data))
     if ty == 'velocity':
@@ -83,45 +77,6 @@ def calculate_pgm(data, ty, delta):
         int2_data = di.integrate_cumtrapz(int_data, delta)
         pgd = max(abs(int2_data))
     return pgd, pgv, pga
-
-
-class NormPE(GenericPE):
-    def __init__(self):
-        GenericPE.__init__(self)
-        self._add_input("input")
-        self._add_output("output_mean")
-        self._add_output("output_max")
-
-    def _process(self, data):
-        stream, filename = data['input']
-        data_mean, data_max, d = calculate_norm(stream)
-        self.write('output_mean', [stream, filename, data_mean, 'mean'])
-        self.write('output_max', [stream, filename, data_max, 'max'])
-
-class PeakGroundMotion(IterativePE):
-    def __init__(self,ty):
-        IterativePE.__init__(self)
-        self.ty=ty
-
-    def _process(self, s_data):
-        stream, filename, data, p_norm = s_data
-        delta = stream[0].stats.delta
-        frequencies = (0.3, 1.0, 3.0)
-        damp  = 0.1
-        pgd, pgv, pga = calculate_pgm(data, self.ty, delta)
-        dmp_spec_acc = {}
-        for freq in frequencies:
-            dmp = calculate_damped_spectral_acc(data, delta, freq, damp, self.ty)
-            dmp_spec_acc['PSA_{}Hz'.format(freq)] = dmp
-
-        self.write('output', [filename, stream, self.ty, {
-            'pgd': pgd.item(),
-            'pgv': pgv.item(),
-            'pga': pga.item(),
-            'dmp_spec_acc': dmp_spec_acc,
-            'p_norm': p_norm
-            }]
-        )
 
 def calculate_damped_spectral_acc(data,delta,freq,damp,ty):
 
@@ -147,36 +102,139 @@ def calculate_damped_spectral_acc(data,delta,freq,damp,ty):
 
     return dmp_spec_acc
 
-class WriteStream(ConsumerPE):
+
+class StreamProducer(IterativePE):
+
+    def __init__(self, label):
+        IterativePE.__init__(self)
+        self.label = label
+
+    def _process(self, input):
+        filename = input
+        self.write('output', [read(filename), self.label])
+
+
+class NormPE(GenericPE):
+    def __init__(self):
+        GenericPE.__init__(self)
+        self._add_input("input")
+        self._add_output("output_mean")
+        self._add_output("output_max")
+
+    def _process(self, data):
+        stream, filename = data['input']
+        data_mean, data_max, d = calculate_norm(stream)
+        self.write('output_mean', [stream, filename, data_mean, 'mean'])
+        self.write('output_max', [stream, filename, data_max, 'max'])
+
+
+class PeakGroundMotion(IterativePE):
+    def __init__(self,ty):
+        IterativePE.__init__(self)
+        self.ty=ty
+
+    def _process(self, s_data):
+        stream, filename, data, p_norm = s_data
+        delta = stream[0].stats.delta
+        frequencies = (0.3, 1.0, 3.0)
+        damp  = 0.1
+        pgd, pgv, pga = calculate_pgm(data, self.ty, delta)
+        dmp_spec_acc = {}
+        for freq in frequencies:
+            dmp = calculate_damped_spectral_acc(data, delta, freq, damp, self.ty)
+            dmp_spec_acc['PSA_{}Hz'.format(freq)] = dmp.item()
+
+        results = {
+            'PGD': pgd.item(),
+            'PGV': pgv.item(),
+            'PGA': pga.item(),
+            'p_norm': p_norm
+        }
+        results.update(dmp_spec_acc)
+        self.write('output', [
+            stream[0].stats.station,
+            filename, stream, self.ty, results]
+        )
+
+
+class Match(GenericPE):
+    def __init__(self):
+        GenericPE.__init__(self)
+        self._add_input('input', grouping=[0])
+        self._add_output('output')
+        self.store = defaultdict(lambda: {})
+
+    def _process(self, data):
+        station, label,stream, ty, pgm = data['input']
+        p_norm = pgm['p_norm']
+        self.store[(station, p_norm)][label] = stream, ty, pgm
+        if len(self.store[(station, p_norm)]) >= 2:
+            print('output: {} {}'.format(station, p_norm))
+            self.write('output', [station, p_norm, self.store[(station, p_norm)]])
+
+
+def comp(real_param, synt_param):
+    result_diff = real_param - synt_param
+    result_rel_diff = (real_param - synt_param)/real_param
+    return result_diff, result_rel_diff
+
+
+class WriteGeoJSON(ConsumerPE):
     def __init__(self):
         ConsumerPE.__init__(self)
 
     def _process(self, data):
-        filename,stream,ty,pgm=data
+        station, p_norm, matching_data = data
+
+        difference = { }
+        relative_difference = {}
+        stream_r, ty_r, pgm_r = matching_data['real']
+        stream_s, ty_s, pgm_s = matching_data['synth']
+        sac = stream_r[0].stats.sac
+        coordinates = [sac.stla, sac.stlo]
+        for param in pgm_r:
+            if param == 'p_norm':
+                continue
+            diff, rel_diff = comp(pgm_r[param], pgm_s[param])
+            difference[param] = diff
+            relative_difference[param] = rel_diff
+
         output_dir="./"
-        output_data={"GroundMotion": {
-            "stream":filename,
-      	    "ty": ty
+        output_data={
+            "type": "Feature",
+            "properties": {
+                "station": station,
+                "data": pgm_r,
+                "synt": pgm_s,
+                "difference": difference,
+                "relative_difference": relative_difference,
+                "geometry": {
+                  "type": "Point",
+                  "coordinates": coordinates
+                }
+            }
         }
-        }
-        output_data['GroundMotion'].update(pgm)
-        self.log("output_data is %s" % json.dumps(output_data))
-        filename="GroundMotion"+"_"+os.path.basename(filename)+".json"
-        with open(filename, 'w') as outfile:
-    	    json.dump(output_data, outfile)
+        self.log("output_data is %s" % output_data)
+        # filename="GroundMotion"+"_"+os.path.basename(filename)+".json"
+        # json.dump(output_data, outfile)
 
 
-streamProducer=StreamProducer()
-streamProducer.name='streamProducer'
+streamProducerReal=StreamProducer('real')
+streamProducerReal.name="streamProducerReal"
+streamProducerSynth=StreamProducer('synth')
+streamProducerSynth.name='streamProducerSynth'
 norm=NormPE()
 pgm_mean=PeakGroundMotion('velocity')
 pgm_max=PeakGroundMotion('velocity')
-write_stream = WriteStream()
+match = Match()
+write_stream = WriteGeoJSON()
 
 
 graph = WorkflowGraph()
-graph.connect(streamProducer, 'output', norm,'input')
+graph.connect(streamProducerReal, 'output', norm,'input')
+graph.connect(streamProducerSynth, 'output', norm,'input')
 graph.connect(norm, 'output_mean', pgm_mean,'input')
 graph.connect(norm, 'output_max', pgm_max,'input')
-graph.connect(pgm_max,'output',write_stream,'input')
-graph.connect(pgm_mean,'output',write_stream,'input')
+graph.connect(pgm_max, 'output', match, 'input')
+graph.connect(pgm_mean, 'output', match, 'input')
+graph.connect(match,'output',write_stream,'input')
