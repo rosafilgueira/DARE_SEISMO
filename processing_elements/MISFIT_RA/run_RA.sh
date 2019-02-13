57d6891d08889b5e549b8fcb75b1f05ef02ec428
rosafilgueira <rosa.filgueira@epcc.ed.ac.uk>
2019-02-13 18:36:55 +0000

    updated script

--- processing_elements/MISFIT_RA/run_RA.sh
+++ processing_elements/MISFIT_RA/run_RA.sh
@@ -1,1 +1,1 @@
-dispel4py simple dispel4py_RA.pgm_story.py -d '{"streamProducerReal": [ {"input": "IV.ARRO.EHR.data"} ], "streamProducerSynth": [ {"input": "IV.ARRO.HXR.synth"} ]   }'
+dispel4py simple dispel4py_RA.pgm_story.py -d '{"streamProducerReal": [ {"input": "./misfit_data/output/IV.ARRO.EHR.data"} ], "streamProducerSynth": [ {"input": "./misfit_data/output/IV.ARRO.HXR.synth"} ]   }'
