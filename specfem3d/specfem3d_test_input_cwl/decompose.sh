95f265ad67c6b3946d4da3d06a539812c2c1e2b0
rosafilgueira <rosa.filgueira@epcc.ed.ac.uk>
2019-05-07 15:27:39 +0100

    removing

--- specfem3d/specfem3d_test_input_cwl/decompose.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# decomposes mesh using the pre-saved mesh files in MESH-default
-echo
-echo "  decomposing mesh..."
-echo
-./bin/xdecompose_mesh $NPROC ./DATA/mesh_homogeneous $BASEMPIDIR
-
