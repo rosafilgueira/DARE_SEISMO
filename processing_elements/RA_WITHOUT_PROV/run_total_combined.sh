1639a3addbdc16a89eb4fdf05cc6dd2be8a5eb69
rosafilgueira <rosa.filgueira@epcc.ed.ac.uk>
2019-09-14 12:29:08 +0100

    improved create_misfit_prep wihtou misfit input

--- processing_elements/RA_WITHOUT_PROV/run_total_combined.sh
+++ processing_elements/RA_WITHOUT_PROV/run_total_combined.sh
@@ -31,7 +31,7 @@ PYTHONPATH=/Users/rosafilgueira/EPCC/DARE/dispel4py:. python -m dispel4py.new.pr
 
 # ####### 4. Get pre-processed synth and data --- Misfit Preprocess
 
-PYTHONPATH=/Users/rosafilgueira/EPCC/DARE/dispel4py:. python -m dispel4py.new.processor simple create_misfit_prep.py -f misfit_input.jsn
+PYTHONPATH=/Users/rosafilgueira/EPCC/DARE/dispel4py:. python -m dispel4py.new.processor simple create_misfit_prep.py
 
 # ####### 5. Get ground motion parameters and compare them
 
