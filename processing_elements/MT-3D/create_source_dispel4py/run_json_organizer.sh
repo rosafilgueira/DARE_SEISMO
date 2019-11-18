6020d8695682d25ae80f35091d72e45438aed663
rosafilgueira <rosa.filgueira@epcc.ed.ac.uk>
2019-11-18 09:33:01 -0700

    new workflow that combines dispel4py_CMTSOLUTION.py and dispel4py_json_organizer.py

--- processing_elements/MT-3D/create_source_dispel4py/run_json_organizer.sh
+++ processing_elements/MT-3D/create_source_dispel4py/run_json_organizer.sh
@@ -10,5 +10,5 @@ type='jorganizer'
 rm -rf $out_url$type
 mkdir $out_url$type
 
-dispel4py simple dispel4py_dispel4py_CMTSOLUTION_total.py  -d '{"json_organizer":[{"file_url": "'$file_url'", "out_url": "'$out_url$type'/"}]}'
+dispel4py simple dispel4py_CMTSOLUTION_total.py  -d '{"json_organizer":[{"file_url": "'$file_url'", "out_url": "'$out_url$type'/"}]}'
 
