1a6a48a5c0557782c67d41760379df28d79b6713
Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2020-03-09 18:47:18 +0000

    Update run_pyflex.sh

--- processing_elements/MT-3D/pyflex/run_pyflex.sh
+++ processing_elements/MT-3D/pyflex/run_pyflex.sh
@@ -9,5 +9,7 @@ export OUTPUT_PYFLEX='./results/'
 rm -rf ./results/MEASURE
 mkdir -p ./results/MEASURE
 
-PYTHONPATH=/Users/rosafilgueira/EPCC/DARE/dispel4py:. python -m dispel4py.new.processor simple pyflex_dispel4py.py
+#PYTHONPATH=/Users/rosafilgueira/EPCC/DARE/dispel4py:. python -m dispel4py.new.processor simple pyflex_dispel4py.py
+
+dispel4py simple  pyflex_dispel4py.py
 
