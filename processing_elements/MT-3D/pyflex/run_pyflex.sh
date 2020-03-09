911b26e06aff3a54d77a4cf04cade63bf8ba173e
Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2020-03-09 19:20:14 +0000

    Update run_pyflex.sh

--- processing_elements/MT-3D/pyflex/run_pyflex.sh
+++ processing_elements/MT-3D/pyflex/run_pyflex.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
-set -x 
+set -x
 
-export PYTHONPATH=$PYTHONPATH:.
-export PYFLEX_RESULTS='./output/'
-export INTERPOLATED_DATA='./interp/'
-export OUTPUT_PYFLEX='./results/'
+#export PYTHONPATH=$PYTHONPATH:.
+
+#Synthetics and observed data from the preprocess step
+export PREP_OUTPUT='./output/'
+
+#Interpolating synthetics and observed data.
+export INPUT_DATA='./input/'
+
+#Results of the pyflex step
+export PYFLEX_OUTPUT='./results/'
+
+rm -rf ./input/data
+rm -rf ./input/synth
+
+mkdir -p ./input/data
+mkdir -p ./input/synth
 
 rm -rf ./results/MEASURE
 mkdir -p ./results/MEASURE
 
-#PYTHONPATH=/Users/rosafilgueira/EPCC/DARE/dispel4py:. python -m dispel4py.new.processor simple pyflex_dispel4py.py
 
-dispel4py simple  pyflex_dispel4py.py
+dispel4py simple pyflex_dispel4py.
+#PYTHONPATH=/Users/rosafilgueira/EPCC/DARE/dispel4py:. python -m dispel4py.new.processor simple pyflex_dispel4py.
 
