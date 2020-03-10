628c61c1507fc6318a0ff4a2a04774bb0443362b
Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2020-03-10 17:48:12 +0000

    Update run_pyflex.sh

--- processing_elements/MT-3D/pyflex/run_pyflex.sh
+++ processing_elements/MT-3D/pyflex/run_pyflex.sh
@@ -1,24 +1,7 @@
 !/bin/bash
 set -x 
 
-export PYTHONPATH=$PYTHONPATH:.
-
-#Synthetics and observed data from the preprocess step
-export PREP_OUTPUT='./output/'
-
-#Interpolating synthetics and observed data.   
-export INPUT_DATA='./input/'
-
-#Results of the pyflex step 
-export PYFLEX_OUTPUT='./results/'
-
-rm -rf ./input/data
-rm -rf ./input/synth
-
-mkdir -p ./input/data
-mkdir -p ./input/synth
-
-rm -rf ./results/MEASURE
-mkdir -p ./results/MEASURE
+# I think the following export is not needed - just in case, I leave it here commented. 
+#export PYTHONPATH=$PYTHONPATH:.
 
 dispel4py simple pyflex_dispel4py.py -d '{"interpolate": [ {"input_data":"input", "prep_output": "output", "pyflex_output": "results" }]}'
