04a3da1807a8963440a7a841856003cdd0bd60f9
Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2020-03-10 17:41:35 +0000

    Update run_pyflex.sh WITH dictionary as an input

--- processing_elements/MT-3D/pyflex/run_pyflex.sh
+++ processing_elements/MT-3D/pyflex/run_pyflex.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
-set -x
+!/bin/bash
+set -x 
 
 export PYTHONPATH=$PYTHONPATH:.
 
 #Synthetics and observed data from the preprocess step
 export PREP_OUTPUT='./output/'
 
-#Interpolating synthetics and observed data.
+#Interpolating synthetics and observed data.   
 export INPUT_DATA='./input/'
 
-#Results of the pyflex step
+#Results of the pyflex step 
 export PYFLEX_OUTPUT='./results/'
 
 rm -rf ./input/data
@@ -21,7 +21,4 @@ mkdir -p ./input/synth
 rm -rf ./results/MEASURE
 mkdir -p ./results/MEASURE
 
-
-dispel4py simple pyflex_dispel4py.py
-
-
+dispel4py simple pyflex_dispel4py.py -d '{"interpolate": [ {"input_data":"input", "prep_output": "output", "pyflex_output": "results" }]}'
