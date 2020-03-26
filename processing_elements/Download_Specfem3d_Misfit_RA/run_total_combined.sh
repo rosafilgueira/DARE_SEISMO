c2ccab224a2878b6fcb8505d96b64b7151f33731
rosafilgueira <rosa.filgueira@epcc.ed.ac.uk>
2020-03-26 15:11:20 +0000

    updating run_total_combined.sh

--- processing_elements/Download_Specfem3d_Misfit_RA/run_total_combined.sh
+++ processing_elements/Download_Specfem3d_Misfit_RA/run_total_combined.sh
@@ -16,6 +16,7 @@ export PYTHONPATH=$PYTHONPATH:.
 export MISFIT_PREP_CONFIG="processing.json"
 export STAGED_DATA="./misfit_data/"
 export OUTPUT="./GM/"
+export PREP_IN_DATA="./misfit_data/events_info.xml"
 ##fm: added for provenance 
 export DOWNL_RUNID="DOWNL-"`uuidgen`
 export PREPOC_RUNID="PREPROC-"`uuidgen`
@@ -32,21 +33,21 @@ export REPOS_URL="http://testbed.project-dare.eu/prov/workflowexecutions/insert"
 
 ######## 2. Create input for download -- it reads the input files of the specfem3d simulation and creates the corresponding input json file for the following download workflow
 
-PYTHONPATH=/Users/rosafilgueira/EPCC/DARE/dispel4py:. python -m dispel4py.new.processor simple create_download_json.py -d '{"WJSON" :
+python -m dispel4py.new.processor simple create_download_json.py -d '{"WJSON" :
 [{"specfem3d_data_url":"https://gitlab.com/project-dare/WP6_EPOS/raw/RA_total_script/processing_elements/Download_Specfem3d_Misfit_RA/data.zip",
 "output":"download_test.json"}]}'
 
 
 ######## 3. Get observed data -- it downloads the observed waveforms and stations xml
-PYTHONPATH=/Users/rosafilgueira/EPCC/DARE/dispel4py:. python -m dispel4py.new.processor simple download_FDSN.py -f download_test.json
+python -m dispel4py.new.processor simple download_FDSN.py -f download_test.json
 
 # ####### 4. Get pre-processed synth and data --- it preprocesses observed and synthetic seismograms
-PYTHONPATH=/Users/rosafilgueira/EPCC/DARE/dispel4py:. python -m dispel4py.new.processor simple create_misfit_prep.py 
+python -m dispel4py.new.processor simple create_misfit_prep.py 
 
 
 # ####### 5. Get ground motion parameters and compare them --- it calculates pgm parameters from observed and synthetic seismograms and compare them
 searchpath="./misfit_data/output/"
-PYTHONPATH=/Users/rosafilgueira/EPCC/DARE/dispel4py:. python -m dispel4py.new.processor simple dispel4py_RA.pgm_story.py -d '{"streamProducerReal": [ {"input":"'$searchpath'" } ], "streamProducerSynth": [ {"input": "'$searchpath'"} ]}'
+python -m dispel4py.new.processor simple dispel4py_RA.pgm_story.py -d '{"streamProducerReal": [ {"input":"'$searchpath'" } ], "streamProducerSynth": [ {"input": "'$searchpath'"} ]}'
 
 # ####### 6. Plot the PGM map --- it plots maps of pgm parameters and of comparisons between data and synth
-PYTHONPATH=/Users/rosafilgueira/EPCC/DARE/dispel4py:. python -m dispel4py.new.processor simple dispel4py_RAmapping.py
+python -m dispel4py.new.processor simple dispel4py_RAmapping.py
